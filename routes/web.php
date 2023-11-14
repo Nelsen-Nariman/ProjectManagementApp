6528b48ee19322d57313b8ed5193f305bb08d50f
Ferryansen <kwanan57@gmail.com>
2023-11-15 00:20:13 +0700

    added worker list features

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +23,6 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware(['auth', 'verified'])->group(function() {
@@ -31,9 +31,11 @@ Route::middleware(['auth', 'verified'])->group(function() {
 
     // Ini untuk user dengan role Admin aja
     Route::middleware('admin')->group(function() {
-        Route::get('/workers', function() {
-            return view('contents.worker-management.worker-list');
-        })->name('workers');
+        Route::get('/workers', [UserController::class, 'index'])->name('workers');
+        Route::get('/workers/search', [UserController::class, 'search'])->name('workers.search');
+        Route::get('/workers/{user_id}', [UserController::class, 'show'])->name('worker.detail');
+
+        Route::delete('/workers/{user_id}', [UserController::class, 'destroy'])->name('worker.destroy');
     });
 });
 
